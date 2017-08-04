a42006de0e113161e82c2aea7e60942533a8527a
Yanek Yuk <yanechednezzar@gmail.com>
2017-08-04 14:52:40 +0300

    App Controller calls for Brands, Categories and Products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :find_brands_and_categories
+
+  def find_brands_and_categories
+    @categories = Category.all
+    @brands = Brand.all
+    @products = Product.find(:all, order: 'id desc', limit: 5)
+  end
 end
