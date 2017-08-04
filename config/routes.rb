3db9eef8e185250f3beb773082854919d4a338ef
Yanek Yuk <yanechednezzar@gmail.com>
2017-08-04 21:34:26 +0300

    Add Admin Model & Migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-
   root 'welcome#index'
 
-  devise_for :users 
+  devise_for :users
+  devise_for :admins
 
   resources :users do
     resources :profiles
   end
 
   resources :products, :categories, :brands
-  
+
   resources :carts
   resources :cart_items
   post '/add_to_cart/:product_id' => 'cart_items#add_to_cart', :as => 'add_to_cart'
-
 end
