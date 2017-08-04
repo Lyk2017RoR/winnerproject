ed8df5d96db0709edd8787614339525fcdffd4e4
Yanek Yuk <yanechednezzar@gmail.com>
2017-08-04 19:43:38 +0300

    Styling, logos, tshirt

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,7 @@
 class CartItemsController < ApplicationController
+    def create
+        @cart_item = CartItem.new
+    end
+
+    
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
     before_action :get_cart, only: %i[show destroy]
     before_action :get_necessary, only: %i[show]
-
+    
     def show;
         @cart_items = CartItem.where(cart_id: @cart.id)
         @cart_items.each { |ci| @cart.total_price += ci.product.price }
