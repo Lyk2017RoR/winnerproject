7c48486bddccffa78415116530658da96d27227f
Kaan Kölköy <kaanklky@gmail.com>
2017-08-04 20:27:27 +0300

    Cart & CartItem add

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,9 +1,14 @@
 class CartItemsController < ApplicationController
   def create
-    user_cart = Cart.find_by(user_id: current_user.id)
+    @user_cart = Cart.where(:user_id => current_user.id, :status => nil)
+    if @user_cart.nil?
+      @user_cart = Cart.create()
+    end
+    @cart_item = CartItem.create(cart_id: @user_cart.id, product_id: @product.id)
   end
 
   def destroy
-    
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy()
   end
 end
