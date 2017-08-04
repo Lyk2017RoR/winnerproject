341a7c228fb6ee969b2b237d96dccd4f975595b6
Yanek Yuk <yanechednezzar@gmail.com>
2017-08-04 20:06:11 +0300

    Paginate Product on welcome page, logo moved to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   def find_brands_and_categories
     @categories = Category.all
     @brands = Brand.all
-    @products = Product.all
+    @products = Product.paginate(page: params[:page], per_page: 8)
   end
 end
