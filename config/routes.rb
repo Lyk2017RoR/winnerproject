e617c80003048bca18cedd00b8b847140cd3f91c
Yanek Yuk <yanechednezzar@gmail.com>
2017-08-04 15:37:13 +0300

    Add routes for products, brands, categories and cart items nested in carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root 'welcome#index'
+
+  devise_for :users
+
+  resources :products, :categories, :brands
+  
+  resources :carts do
+    resources :cart_items
+  end
+
 end
