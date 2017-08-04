faa0d01164b68a030874d6828022fcb9375b4b43
Kaan Kölköy <kaanklky@gmail.com>
2017-08-04 15:27:38 +0300

    Brands, Categories & Products' controllers

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,38 @@
 class BrandsController < ApplicationController
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(params.require(:brand).permit(:name))
+
+    if @brand.save
+      redirect_to welcome_admin_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @brand = Brand.find(params[:id])    
+  end
+
+  def edit
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+    if @brand.update(params.require(:brand).permit(:title))
+      redirect_to welcome_admin_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @brand = Brand.find(params[:id])
+    @brand.destroy
+    redirect_to welcome_admin_path    
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,38 @@
 class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params.require(:category).permit(:name, :image))
+
+    if @category.save
+      redirect_to welcome_admin_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])    
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(params.require(:category).permit(:name, :image))
+      redirect_to welcome_admin_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to welcome_admin_path    
+  end
 end
