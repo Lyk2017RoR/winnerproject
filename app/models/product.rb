03a5fede7086e01d1ec91fb8577f5092114abb61
Kaan Kölköy <kaanklky@gmail.com>
2017-08-05 11:15:59 +0300

    Paperclip & watermark

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,10 +23,25 @@ class Product < ApplicationRecord
     attachment.instance.category.id
   end
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/:category_id.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
-  # validates_attachment_content_type :image, content_type: /^image\/(jpg|jpeg)$/
+  # has_attached_file :image, :path => "/products/:id/:id_large.jpg", styles: { medium: "500x500" }, default_url: "/images/:style/:category_id.png"
+  # has_attached_file :image,
+  #   :processors => [:watermark],
+  #   :path => "/products/:id/:id_large.jpg",
+  #   :default_url => "/images/:style/:category_id.png",
+  #   :styles => {
+  #     :large => '500x500',
+  #     :content => {
+  #       :geometry => '700>',
+  #       :watermark_path => "#{Rails.root}/public/images/watermark.png",
+  #       :position => 'SouthWest'
+  #     },
+  #   }
+  has_attached_file :image, :processors => [:watermark],
+  :styles => { :original => { :geometry => '600x515#', :watermark_path => "#{Rails.root}/public/images/original/watermark.png" } },
+  :url    => '/assets/attachment/:id/:style/:basename.:extension',
+  :path   => '/products/:id/:id_large.jpg',
+  :default_url => "/images/:style/:category_id.png"
+  validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg)$/
 
   extend FriendlyId
   friendly_id :name, use: :slugged
